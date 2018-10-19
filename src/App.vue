12b7c6a7bd01156c9bf2d0e90d65a69235841355
wuhaoming <369750231@qq.com>
2018-10-19 09:35:59 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <p>移动端图片上传预览组件：</p>
+    <Upload max="3" @listenToUpload="receiveUpload"/>
+  </div>
+</template>
+
+<script>
+import Upload from './components/Upload'
+
+export default {
+  name: 'App',
+  components: {
+    Upload
+  },
+  methods:{
+    receiveUpload (data) {
+      console.log(data)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
